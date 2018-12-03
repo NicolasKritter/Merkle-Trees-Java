32f20eaf40f5642f32bf72f59a4263b870330335
Nicolas Kritter <nicolas.kritter@isep.fr>
2018-12-03 11:56:42 +0100

    fixes

--- src/interfaces/LogServerInterface.java
+++ src/interfaces/LogServerInterface.java
@@ -1,12 +1,13 @@
 package interfaces;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.List;
 
 public interface LogServerInterface extends Remote{
-	void appendEvent(List<String> logs);
-	byte[] getRootHash();
-	List<byte[]> genPath(int i);
-	List<byte[]> genProof(int i);
+	void appendEvent(List<String> logs)throws RemoteException;;
+	byte[] getRootHash()throws RemoteException;;
+	List<byte[]> genPath(int i)throws RemoteException;;
+	List<byte[]> genProof(int i)throws RemoteException;;
 	
 }
